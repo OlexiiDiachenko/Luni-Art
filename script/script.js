992d32b91e127a35e922da1e99d6127b511c5555
Oleksii Diachenko <elexeeen@gmail.com>
2022-10-13 18:42:00 +0300

    modified:   script/script.js

--- script/script.js
+++ script/script.js
@@ -1,307 +1,314 @@
-$(document).ready(function(){
-function imageComparison(selector){
-    let comparison = $(selector)
-        .addClass('image-comparison')
+$(document).ready(function () {
+  function imageComparison(selector) {
+    console.log(selector);
+    let comparison = $(selector).addClass("image-comparison");
 
     let images = comparison
-        .find('img')
-        .addClass('image-comparison_image')
-        .css('max-width' , comparison.width());
+      .find("img")
+      .addClass("image-comparison_image")
+      .css("max-width", comparison.width());
 
     let before = comparison
-        .find('.image-comparison_before')
-        .append(images.eq(0))
+      .find(".image-comparison_before")
+      .append(images.eq(0));
 
     comparison
-        .find('.image-comparison_slider')
-        .on('dragstart' , () => false)
-        .on('mousedown' , function (e) {
-            let slider = $(this);
+      .find(".image-comparison_slider")
+      .on("dragstart", () => false)
+      .on("mousedown", function (e) {
+        let slider = $(this);
 
-            let doc = $(document).on('mousemove' , (e) => {
-                let offset = e.pageX - comparison.position().left;
-                let width = comparison.width();
+        let doc = $(document).on("mousemove", (e) => {
+          let offset = e.pageX - comparison.position().left;
+          let width = comparison.width();
 
-                if(offset < 0) offset = 0;
-                if(offset > width) offset = width;
+          if (offset < 0) offset = 0;
+          if (offset > width) offset = width;
 
-                slider.css('left' , offset + 'px');
-                before.css('width' , offset + 'px');
-
-            });
-
-            doc.on('mouseup', () => doc.off('mousemove'));
+          slider.css("left", offset + "px");
+          before.css("width", offset + "px");
         });
-}
 
-imageComparison('#image-comparison');
-imageComparison('#image-comparison_down');
+        doc.on("mouseup", () => doc.off("mousemove"));
+      })
+      .on("touchstart", function (e) {
+        let slider = $(this);
 
-    $('.slider_photo').slick({
-        slidesToShow:3,
-        speed:1000,
-        easing:'ease',
-        draggable:false,
-    });
+        let doc = $(document).on("touchmove", (e) => {
+          let offset = e.pageX - comparison.position().left;
+          let width = comparison.width();
 
-    $('.slider_size').slick({
-        dots:true,
-        slidesToShow:2,
-        speed:1000,
-        easing:'ease',
-        draggable:false,
-    });
+          if (offset < 0) offset = 0;
+          if (offset > width) offset = width;
 
-    $('.slider_video').slick({
-        slidesToShow:3,
-        speed:1000,
-        easing:'ease',
-        draggable:false,
-    });
-})
+          slider.css("left", offset + "px");
+          before.css("width", offset + "px");
+        });
+
+        doc.on("touchend", () => doc.off("touchmove"));
+      });
+  }
+
+  imageComparison("#image-comparison");
+  imageComparison("#image-comparison_down");
+
+  $(".slider_photo").slick({
+    slidesToShow: 3,
+    speed: 1000,
+    easing: "ease",
+    draggable: false,
+  });
+
+  $(".slider_size").slick({
+    dots: true,
+    slidesToShow: 2,
+    speed: 1000,
+    easing: "ease",
+    draggable: false,
+  });
+
+  $(".slider_video").slick({
+    slidesToShow: 3,
+    speed: 1000,
+    easing: "ease",
+    draggable: false,
+  });
+});
 
 // Function for Material Cost
 
-let coal = document.querySelector('.coal input');
-let coalLabel = document.querySelector('.coal label');
-let coalPrice = document.querySelector('.choose-grid_coal');
-let oil = document.querySelector('.oil input');
-let oilLabel = document.querySelector('.oil label');
-let oilPrice = document.querySelector('.choose-grid_oil');
-let people = document.querySelectorAll('.humans-count-item');
-let peopleChoose = document.querySelectorAll('.humans-count-label');
+let coal = document.querySelector(".coal input");
+let coalLabel = document.querySelector(".coal label");
+let coalPrice = document.querySelector(".choose-grid_coal");
+let oil = document.querySelector(".oil input");
+let oilLabel = document.querySelector(".oil label");
+let oilPrice = document.querySelector(".choose-grid_oil");
+let people = document.querySelectorAll(".humans-count-item");
+let peopleChoose = document.querySelectorAll(".humans-count-label");
 let totalCountPeople = 1;
 let arrCoalCost = [];
 let arrOilCost = [];
 
-coalPrice.classList.add('active_material');
+coalPrice.classList.add("active_material");
 percentCost(coalPrice);
 
-if ( !coal.checked ){
-    coalLabel.addEventListener('click' , coalTable);
-}else{
-    percentCost(coalPrice);
+if (!coal.checked) {
+  coalLabel.addEventListener("click", coalTable);
+} else {
+  percentCost(coalPrice);
 }
 
-if ( !oil.checked ){
-    oilLabel.addEventListener('click' , oilTable);
-}else { 
-    percentCost(oilPrice);
+if (!oil.checked) {
+  oilLabel.addEventListener("click", oilTable);
+} else {
+  percentCost(oilPrice);
 }
 
-for ( let i = 0 ; i < peopleChoose.length ; i++ ){
-    peopleChoose[i].addEventListener('click' , newCount);
+for (let i = 0; i < peopleChoose.length; i++) {
+  peopleChoose[i].addEventListener("click", newCount);
 }
 
-function newCount() { 
-    let countPeople = this.previousElementSibling.getAttribute('data-count');
-    if ( coal.checked ){
-        newCost(coalPrice , countPeople , arrCoalCost )
+function newCount() {
+  let countPeople = this.previousElementSibling.getAttribute("data-count");
+  if (coal.checked) {
+    newCost(coalPrice, countPeople, arrCoalCost);
+  }
+
+  if (oil.checked) {
+    newCost(oilPrice, countPeople, arrOilCost);
+  }
+}
+
+function coalTable() {
+  coalPrice.classList.add("active_material");
+  if (oilPrice.classList.contains("active_material")) {
+    oilPrice.classList.remove("active_material");
+  }
+  peopleCount();
+
+  newCost(coalPrice, totalCountPeople, arrCoalCost);
+  percentCost(coalPrice);
+}
+
+function oilTable() {
+  coal.removeAttribute("checked");
+  oilPrice.classList.add("active_material");
+  if (coalPrice.classList.contains("active_material")) {
+    coalPrice.classList.remove("active_material");
+  }
+  peopleCount();
+  newCost(oilPrice, totalCountPeople, arrOilCost);
+  percentCost(oilPrice);
+}
+
+// Function for reading a count people.
+function peopleCount() {
+  // Array with a peoples ( green peoples on website. ) Our radioinputs
+
+  for (i = 0; i < people.length; i++) {
+    // If we found a people, who has checked status, we need get his Number, our data-count
+
+    if (people[i].checked) {
+      totalCountPeople = people[i].getAttribute("data-count");
+
+      // Else , we checked next
+    } else {
+      continue;
     }
-    
-    if ( oil.checked ){
-        newCost(oilPrice , countPeople , arrOilCost )
-    }
+  }
 }
 
-function coalTable(){
-    coalPrice.classList.add('active_material');
-    if(oilPrice.classList.contains('active_material')){
-        oilPrice.classList.remove('active_material');
+function percentCost(materialPercent) {
+  let size = materialPercent.querySelectorAll(".size_choose");
+  for (let i = 0; i < size.length; i++) {
+    let cost = size[i].querySelector(".cost");
+    let percent = size[i].querySelector(".percent");
+    if (percent == null) {
+      continue;
+    } else {
+      percent.innerHTML = parseInt(cost.innerHTML) * 0.2 + "₴";
     }
-    peopleCount();
-    
-    newCost(coalPrice , totalCountPeople , arrCoalCost )
-    percentCost(coalPrice);
-
+  }
 }
 
-function oilTable(){
-    coal.removeAttribute('checked');
-    oilPrice.classList.add('active_material');
-    if(coalPrice.classList.contains('active_material')){
-        coalPrice.classList.remove('active_material');
+function newCost(tablePrice, count, arrCost) {
+  let NewCost = tablePrice.querySelectorAll(".size_choose");
+  for (let i = 0; i < NewCost.length; i++) {
+    let a = NewCost[i];
+    let cost = a.querySelector(".cost");
+    if (cost == null) {
+      continue;
+    } else {
+      arrCost.push(parseInt(cost.innerHTML));
+      if (arrCost.length > 5) {
+        arrCost.pop();
+      }
     }
-    peopleCount();
-    newCost(oilPrice , totalCountPeople , arrOilCost)
-    percentCost(oilPrice);
-}
+  }
 
-
-// Function for reading a count people. 
-function peopleCount(){ 
-
-    // Array with a peoples ( green peoples on website. ) Our radioinputs
-
-    for ( i = 0 ; i < people.length ; i++ ){ 
-
-        // If we found a people, who has checked status, we need get his Number, our data-count
-
-        if ( people[i].checked ){
-            totalCountPeople = people[i].getAttribute('data-count');
-
-        // Else , we checked next
-
-        } else { 
-            continue;
+  for (let j = 0; j < NewCost.length; j++) {
+    let cost = NewCost[j].querySelector(".cost");
+    if (j == 0) {
+      if (count > 2) {
+        NewCost[j].classList.add("unactive");
+        cost.innerHTML = cost.innerHTML;
+      } else {
+        if (NewCost[j].classList.contains("unactive")) {
+          NewCost[j].classList.remove("unactive");
         }
-    }
-}
-
-function percentCost(materialPercent) { 
-    let size = materialPercent.querySelectorAll('.size_choose');
-    for ( let i = 0 ; i < size.length ; i++ ){
-        let cost = size[i].querySelector('.cost');
-        let percent = size[i].querySelector('.percent');
-        if ( percent == null ){
-            continue;
-        }else{
-            percent.innerHTML = parseInt(cost.innerHTML) * 0.2 + '₴';
+        cost.innerHTML = arrCost[j] + (count - 1) * 150;
+      }
+    } else if (j == 1) {
+      if (count > 4) {
+        NewCost[j].classList.add("unactive");
+        cost.innerHTML = cost.innerHTML;
+      } else {
+        if (NewCost[j].classList.contains("unactive")) {
+          NewCost[j].classList.remove("unactive");
         }
-    }
-}
-
-function newCost(tablePrice , count , arrCost ) { 
-
-    let NewCost = tablePrice.querySelectorAll('.size_choose');
-    for ( let i = 0 ; i < NewCost.length ; i++ ){ 
-        let a = NewCost[i];
-        let cost = a.querySelector('.cost');
-        if ( cost == null ){
-            continue;
-        }else{
-            arrCost.push(parseInt(cost.innerHTML));
-            if ( arrCost.length > 5 ){
-                arrCost.pop()
-            }
+        cost.innerHTML = arrCost[j] + (count - 1) * 200;
+      }
+    } else if (j == 2) {
+      if (count > 6) {
+        NewCost[j].classList.add("unactive");
+        cost.innerHTML = cost.innerHTML;
+      } else {
+        if (NewCost[j].classList.contains("unactive")) {
+          NewCost[j].classList.remove("unactive");
         }
+        cost.innerHTML = arrCost[j] + (count - 1) * 300;
+      }
+    } else if (j == 3) {
+      cost.innerHTML = arrCost[j] + (count - 1) * 400;
+    } else if (j == 4) {
+      cost.innerHTML = arrCost[j] + (count - 1) * 500;
     }
- 
-    for ( let j = 0 ; j < NewCost.length ; j++ ){
-        let cost = NewCost[j].querySelector('.cost');
-        if( j == 0 ){
-            if ( count > 2 ){
-                NewCost[j].classList.add('unactive');
-                cost.innerHTML = cost.innerHTML;
-            }else{
-                if ( NewCost[j].classList.contains('unactive') ){
-                    NewCost[j].classList.remove('unactive')
-                }
-                cost.innerHTML = arrCost[j] + ((count-1) * 150);
-            }
-        }else if( j == 1 ){
-            if ( count > 4 ){
-                NewCost[j].classList.add('unactive');
-                cost.innerHTML = cost.innerHTML;
-            }else{
-                if ( NewCost[j].classList.contains('unactive') ){
-                    NewCost[j].classList.remove('unactive')
-                }
-                cost.innerHTML = arrCost[j] + ((count-1) * 200);
-            }
-        }else if( j == 2 ){
-            if ( count > 6 ){
-                NewCost[j].classList.add('unactive');
-                cost.innerHTML = cost.innerHTML;
-            }else{
-                if ( NewCost[j].classList.contains('unactive') ){
-                    NewCost[j].classList.remove('unactive')
-                }
-                cost.innerHTML = arrCost[j] + ((count-1) * 300);
-            }
-        }else if( j == 3 ){
-            cost.innerHTML = arrCost[j] + ((count-1) * 400);
-        }else if( j == 4 ){
-            cost.innerHTML = arrCost[j] + ((count-1) * 500);
-        }
-    }
-    let activeMaterial = document.querySelector('.active_material');
-    percentCost(activeMaterial);
+  }
+  let activeMaterial = document.querySelector(".active_material");
+  percentCost(activeMaterial);
 }
-
-
 
 // Order Section on/off ( Last form )
 
-let order = document.querySelectorAll('.order_button');
-let consult = document.querySelector('.consult');
-let close = document.querySelector('.close');
+let order = document.querySelectorAll(".order_button");
+let consult = document.querySelector(".consult");
+let close = document.querySelector(".close");
 
-for ( let i = 0 ; i < order.length ; i++ ){ 
-    order[i].addEventListener('click' , showOrder);
+for (let i = 0; i < order.length; i++) {
+  order[i].addEventListener("click", showOrder);
 }
 
 function showOrder() {
-    consult.style.display = 'flex';
+  consult.style.display = "flex";
 }
 
-close.addEventListener('click' , closeOrder);
+close.addEventListener("click", closeOrder);
 
 function closeOrder() {
-    consult.style.display = 'none';
+  consult.style.display = "none";
 }
 
 // A little validate a form in order menu
 
-const phone = document.querySelector('#phone');
-const name = document.querySelector('#name');
-let inputsConsult = document.querySelectorAll('.userInfo-input');
+const phone = document.querySelector("#phone");
+const name = document.querySelector("#name");
+let inputsConsult = document.querySelectorAll(".userInfo-input");
 
-for ( let i = 0 ; i < inputsConsult.length ; i++ ){ 
-    inputsConsult[i].addEventListener('blur' , validate);
-    inputsConsult[i].addEventListener('input' , validate)
+for (let i = 0; i < inputsConsult.length; i++) {
+  inputsConsult[i].addEventListener("blur", validate);
+  inputsConsult[i].addEventListener("input", validate);
 }
 
-function validate() { 
-    let lengthWrite = parseInt(this.value.length);
-    let validateLength = this.getAttribute('data-length');
-    if ( lengthWrite < validateLength ){
-        this.style.borderBottom = '1px solid rgba(255 , 0 , 0 , 0.25)';
-        if ( this.parentElement.classList.contains('valid') ){
-            this.parentElement.classList.remove('valid');
-        }
-    }else{
-        this.style.borderBottom = '1px solid rgba(0 , 255 , 0 , 0.25)';
-        this.parentElement.classList.add('valid');
+function validate() {
+  let lengthWrite = parseInt(this.value.length);
+  let validateLength = this.getAttribute("data-length");
+  if (lengthWrite < validateLength) {
+    this.style.borderBottom = "1px solid rgba(255 , 0 , 0 , 0.25)";
+    if (this.parentElement.classList.contains("valid")) {
+      this.parentElement.classList.remove("valid");
     }
+  } else {
+    this.style.borderBottom = "1px solid rgba(0 , 255 , 0 , 0.25)";
+    this.parentElement.classList.add("valid");
+  }
 }
 
-phone.addEventListener('focus' , startNubmer);
-phone.addEventListener('blur' , removeNubmer);
+phone.addEventListener("focus", startNubmer);
+phone.addEventListener("blur", removeNubmer);
 
-function startNubmer() { 
-    if ( this.value.length > 4 ){
-        this.value = this.value;
-    }else{
-        this.value = '+' + 380;
-    }
+function startNubmer() {
+  if (this.value.length > 4) {
+    this.value = this.value;
+  } else {
+    this.value = "+" + 380;
+  }
 }
 
-function removeNubmer() { 
-    if ( this.value == +380 ){
-        this.value = '';
-    } else { 
-        this.value = this.value;
-    }
+function removeNubmer() {
+  if (this.value == +380) {
+    this.value = "";
+  } else {
+    this.value = this.value;
+  }
 }
 
 // Choose the massanger
 
-let massangersImages = document.querySelectorAll('.massanger_images');
+let massangersImages = document.querySelectorAll(".massanger_images");
 
-for ( let i = 0 ; i < massangersImages.length ; i++ ){ 
-    massangersImages[i].addEventListener('click' , massangerChoose);
+for (let i = 0; i < massangersImages.length; i++) {
+  massangersImages[i].addEventListener("click", massangerChoose);
 }
 
 function massangerChoose() {
-    for ( let j = 0 ; j < massangersImages.length ; j++ ){ 
-        if ( !massangersImages[j].classList.contains('unactive_massanger') ){
-            massangersImages[j].classList.add('unactive_massanger')
-        }else{
-            continue;
-        }
+  for (let j = 0; j < massangersImages.length; j++) {
+    if (!massangersImages[j].classList.contains("unactive_massanger")) {
+      massangersImages[j].classList.add("unactive_massanger");
+    } else {
+      continue;
     }
-    if ( this.classList.contains('unactive_massanger') ){
-        this.classList.remove('unactive_massanger');
-    }
+  }
+  if (this.classList.contains("unactive_massanger")) {
+    this.classList.remove("unactive_massanger");
+  }
 }
