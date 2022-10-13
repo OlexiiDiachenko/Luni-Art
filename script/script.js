9f95498e0bb17d3e3458d5f5e478cadce80dc7fc
Oleksii Diachenko <elexeeen@gmail.com>
2022-10-14 01:23:39 +0300

    modified:   index.html 	new file:   media/mini-tablet.css 	modified:   script/script.js 	modified:   style/roots.css

--- script/script.js
+++ script/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   function imageComparison(selector) {
-    console.log(selector);
     let comparison = $(selector).addClass("image-comparison");
 
     let images = comparison
@@ -30,22 +29,6 @@ $(document).ready(function () {
         });
 
         doc.on("mouseup", () => doc.off("mousemove"));
-      })
-      .on("touchstart", function (e) {
-        let slider = $(this);
-
-        let doc = $(document).on("touchmove", (e) => {
-          let offset = e.pageX - comparison.position().left;
-          let width = comparison.width();
-
-          if (offset < 0) offset = 0;
-          if (offset > width) offset = width;
-
-          slider.css("left", offset + "px");
-          before.css("width", offset + "px");
-        });
-
-        doc.on("touchend", () => doc.off("touchmove"));
       });
   }
 
@@ -312,3 +295,30 @@ function massangerChoose() {
     this.classList.remove("unactive_massanger");
   }
 }
+
+let imageComparison = document.querySelector("#image-comparison");
+let imageComparisonDown = document.querySelector("#image-comparison_down");
+let buttonComparison = document.querySelectorAll(".image-comparison_slider");
+
+buttonComparison.forEach((button) => {
+  button.addEventListener("touchmove", (evt) => {
+    let touches = evt.changedTouches;
+    let newLeft = getComputedStyle(imageComparison.parentElement).paddingLeft;
+    let width = parseFloat(getComputedStyle(imageComparison).width);
+    let offset = touches[0].clientX - parseFloat(newLeft);
+    if (offset >= width) {
+      offset = width;
+    }
+    if (offset < 0) offset = 0;
+    button.style.left = `${offset}px`;
+    button.addEventListener("touchend", () => {
+      let changeImage = button.parentElement.querySelector(
+        ".image-comparison_before img"
+      );
+      let width = parseFloat(getComputedStyle(imageComparison).width);
+      let newWidth = width - parseFloat(getComputedStyle(changeImage).width);
+      changeImage.style.width = ` ${newWidth}px`;
+      console.log(width);
+    });
+  });
+});
